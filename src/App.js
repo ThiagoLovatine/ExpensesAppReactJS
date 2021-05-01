3f3bd5afd5ca67b4c3fa4a2e5225e3d36c21ae7d
Thiago Lovatine <thiagolovatine@gmail.com>
2021-05-01 13:20:07 -0300

    Expense date

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title: "asfc235sd",
+      amount: 435.34,
+      date: new Date(2021, 2, 20),
+      id: "1",
+    },
+    {
+      title: "345v34556",
+      amount: 125.35,
+      date: new Date(2021, 2, 20),
+      id: "2",
+    },
+    {
+      title: "etrune56a",
+      amount: 672.73,
+      date: new Date(2021, 2, 20),
+      id: "3",
+    },
+  ];
+
   return (
     <div className="App">
-      <ExpenseItem />
+      {expenses.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import './ExpenseItem.css';
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
-    return (
-        <div className="expense-item">
-            <div>Date</div>
-            <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
-            </div>
-        </div>
-    )
+export default function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">R${props.amount}</div>
+      </div>
+    </div>
+  );
 }
